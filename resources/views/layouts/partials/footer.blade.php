81031cbcf5aadc4098c8db2cddbd22659583cac2
Mohamed Ali <azhasoft@gmail.com>
2024-12-31 11:23:40 +0200

    Test-1

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <!-- Main Footer -->
   <footer class="main-footer no-print" style="width: 100%;text-align: center">
     <small>
-        Prosoft erp  - V{{config('author.app_version')}} | Copyright &copy; {{ date('Y') }} All rights reserved
+        AZHA SOFT  - V{{config('author.app_version')}} | Copyright &copy; {{ date('Y') }} All rights reserved
     </small>
 </footer>
