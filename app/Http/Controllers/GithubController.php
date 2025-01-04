aea9c4413cbedf29ddd8eec56e93f7faed93133d
Mohamed Ali <azhasoft@gmail.com>
2025-01-04 15:14:58 +0200

    Test Git model

--- app/Http/Controllers/GithubController.php
+++ app/Http/Controllers/GithubController.php
@@ -48,7 +48,7 @@ class GithubController extends Controller
         }
 
 
-       // GithubLog::create($output);
+        GithubLog::create($output);
         return response()-json($output);
 
     }
