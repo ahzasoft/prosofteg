b2d7c242e870a56cf24c41253aab54841663219a
Mohamed Ali <azhasoft@gmail.com>
2025-01-08 13:51:09 +0200

    Add repo Path

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <!-- Main Footer -->
   <footer class="main-footer no-print" style="width: 100%;text-align: center">
     <small>
-        Prosoft EG - V{{config('author.app_version')}} | Copyright &copy; {{ date('Y') }} All rights reserved
+        Prosoft EG (2025) - V{{config('author.app_version')}} | Copyright &copy; {{ date('Y') }} All rights reserved
     </small>
 </footer>
